20e88b9949748892510e2a766784688bbbc5da2f
Suki <sukileung246@gmail.com>
2020-12-03 10:05:07 -0800

    Implemented fib algo

--- GitPracticeProject.py
+++ GitPracticeProject.py
@@ -3,7 +3,12 @@
 # Practice project to demonstrate git workflow
 
 def main():
-    pass
+    # find the nth fibonacci number
+    #eg. 1, 1, 2, 3, 5, 8, 13, 21, 34,...
+    n = int(input("What fibonacci number do u want to find?"))
+
+    if n in [1, 2]:
+        print(f"The{n}st/nd Fib number is 1")
 
 if __name__ == "__main__":
     main()
