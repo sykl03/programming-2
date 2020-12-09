359f5b53ae7bc7dd26ac984e0bfde3ac2631b05a
Suki <sukileung246@gmail.com>
2020-12-09 11:10:33 -0800

    updated!new quiz

--- quiz.py
+++ quiz.py
@@ -1,42 +1,62 @@
 # Math Quiz
-print("Hello User, This is a math quiz. We will be testing your skills! Get ready...")
+# Ask for user's name
+name = input("HEloo what's your name?")
+print(f"Hi {name}! Nice to meet you!")
+# Introduce the quiz
+print("This is a math quiz. We will be testing your skills! Get ready... ")
 counter = 0
-question_one = float(input("What is 2 + 5?"))
-if question_one == 7:
+
+
+# Question one
+question_one = input("What is 2 + 5?")
+q1 = question_one.strip(" ,.!0")
+if q1 == "7":
     print("Correct!")
-    counter = 0 +1
+    counter = 0 + 1
 else:
     print("Sorry that is incorrect :(")
 
 
-question_two = float(input("Name the first first three digits of pi."))
+# Question two
+question_two = float(input("Name the first first three digits of pi. (_.__)"))
 if question_two == 3.14:
     print("oooo that is correct")
     counter = counter + 1
 else:
     print("sorry you're just a little wrong")
 
-question_three = float(input("What is sin^2x + cos^2x equal to?"))
-if question_three == 1:
+
+# Question three
+question_three = input("What is sin^2x + cos^2x equal to?")
+q3 = question_three.strip(" .,!0")
+if q3 == "1":
     print("wowoowowowowowow I see you have taken jmah's math class")
     counter = counter + 1
 else:
     print("hmmm sorry i think you need to brush up on some skills..")
 
-question_four =float(input("What is 4 * 12"))
-if question_four == 48:
+
+# Question four
+question_four =input("What is 4 * 12")
+q4= question_four.strip(" .,!0")
+if q4 == "48":
     print("righteo")
     counter = counter + 1
 else:
     print("incorrect.")
 
-question_five = float(input("square root of 169"))
-if question_five == 13:
+
+# Question Five
+question_five = input("square root of 169")
+q5 = question_five.strip(" .,!0")
+if q5 == "13":
     print("so smart oh my, correct")
     counter = counter + 1
 else:
     print("c'mon man this is so easy, incorrect")
 
+
+# Results and comments!
 print(f"results:{counter}/5")
 results = counter / 5 * 100
 print(f"{results}% is your mark buddy")
